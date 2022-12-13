4c6750504ba0de92ceff73544b67fd11aa45a864
jaredbare <jab468@byu.edu>
2022-12-13 12:52:50 -0700

    CSRF_TRUSTED_ORIGINS

--- Wiki/settings.py
+++ Wiki/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['web-production-a6ea.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-a6ea.up.railway.app']
 
 
 # Application definition
