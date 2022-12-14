61b6e5a41e481da6957435972d6e2900271b3986
davemurdock55 <davemurdock55@gmail.com>
2022-12-14 01:12:35 -0700

    commented out the tinymce url

--- Wiki/urls.py
+++ Wiki/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('wikiWebsite.urls')),
-    path(r'^tinymce/', include('tinymce.urls')),
+    # path(r'^tinymce/', include('tinymce.urls')),
 ]
